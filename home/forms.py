01cdabac7dc190a14726d401e4385b5039fdaccf
Aidas Bendoraitis <aidasbend@yahoo.com>
2022-10-20 02:28:12 +0200

    🎨 Nicer styling.

--- home/forms.py
+++ home/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout
+from crispy_forms.bootstrap import FieldWithButtons
+from crispy_forms.layout import Layout, Submit
 from crispy_bootstrap5 import bootstrap5
+
 
 class SearchForm(forms.Form):
     q = forms.CharField(label="Search", required=False)
@@ -11,20 +13,25 @@ class SearchForm(forms.Form):
         js = [
             "js/search.js",
         ]
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.fields["q"].label = ""
 
         self.helper = FormHelper()
         self.helper.form_tag = True
         self.helper.form_method = "GET"
         self.helper.include_media = False
         self.helper.layout = Layout(
-            bootstrap5.Field("q", type="search"),
+            FieldWithButtons(
+                bootstrap5.Field("q", type="search"),
+                Submit("search", "Search"),
+            )
         )
 
 
 class LoginForm(AuthenticationForm):
-
     class Media:
         js = [
             "js/login.js",
@@ -37,6 +44,6 @@ class LoginForm(AuthenticationForm):
         self.helper.form_tag = False
         self.helper.include_media = False
         self.helper.layout = Layout(
-            bootstrap5.Field("username", autocomplete="username"),
-            bootstrap5.Field("password", autocomplete="current-password"),
+            bootstrap5.FloatingField("username", autocomplete="username"),
+            bootstrap5.FloatingField("password", autocomplete="current-password"),
         )
