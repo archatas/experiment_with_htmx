15c6f52c0e6f703c419decda93ed2a4387282101
Aidas Bendoraitis <aidasbend@yahoo.com>
2022-10-21 23:35:03 +0200

    🔨 JavaScript code style made consistent.

--- experiment_with_htmx/static/js/dialog.js
+++ experiment_with_htmx/static/js/dialog.js
@@ -4,7 +4,7 @@ function init_widgets_for_htmx_element(target) {
     // init modal dialogs
     if (target.tagName === 'DIALOG') {
         target.showModal();
-        htmx.on(".close-dialog", "click", function(event) {
+        htmx.on('.close-dialog', 'click', function(event) {
             var dialog = htmx.find('dialog[open]');
             dialog.close();
             htmx.remove(dialog);
